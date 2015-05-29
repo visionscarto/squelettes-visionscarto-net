f7cd7d0f228ff5846496bbb75b7c047b89d0efbc
Fil <fil@rezo.net>
2015-05-30 00:35:19 +0200

    gerer les videos responsive (necessite un gros patch sur autoembed pour geer les videos youtube… a revoir

--- js/script.js
+++ js/script.js
@@ -7,4 +7,8 @@ $(function() {
   $('.ui.accordion').accordion();
 
   $('.content a').filter(function() { return $('img', this).size(); }).addClass('aimg');
+
+  // resize embedded videos
+  $('article').fitVids();
+
 });
