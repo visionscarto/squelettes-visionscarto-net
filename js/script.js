5c5dded41f88b1b73d6f381b3810475ca09bdfa2
martinarnaud <arno@rezo.net>
2017-03-14 12:27:59 +0100

    Pied de page: 5 articles en «Lire aussi»
    Le script n’est pas bon, mais là je travaille le graphisme, je ne sais
    pas trop comment fonctionne le système de «next», qui est en PHP.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 $(function() {
 
+/*
   $(window).on('scroll resize',
     function() { $('#footer').css({height: $(window).height()+'px'}); }
   );
+  */
   
 
   $('.content a').filter(function() { return $('img', this).size(); }).addClass('aimg');
