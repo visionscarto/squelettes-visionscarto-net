06a930ca3d57d5055856e7d6f02939ef896c7162
martinarnaud <arno@rezo.net>
2017-03-14 11:11:55 +0100

    Pas de Jquery Inline
    Virer le JS inline, dont je ne vois pas l’utilité, et qui interdit de
    charger le JS en asynchrone.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ $(function() {
     function() { $('#footer').css({height: $(window).height()+'px'}); }
   );
   
-  $('.ui.accordion').accordion();
 
   $('.content a').filter(function() { return $('img', this).size(); }).addClass('aimg');
 
